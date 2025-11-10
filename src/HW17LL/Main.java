8db3a13367b42e3d0763414ac26e451de560bdbe
nat_soloviova <natsolovyova2@gmail.com>
2025-11-10 22:39:50 +0100

    first commit

---
+++ src/HW17LL/Main.java
@@ -0,0 +1,48 @@
+package HW17LL;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<String> productList = new LinkedList<String>();
+        addNewProductFirst("Сир", productList);
+        addNewProductFirst("яблука", productList);
+        addNewProductFirst("хліб", productList);
+        printAllProducts(productList);
+        System.out.println("lllllll");
+        addProductByPosition(productList, 2, "second position");
+        printAllProducts(productList);
+        removeByName(productList,"яблука");
+        printAllProducts(productList);
+        buyProductAndDelete(productList);
+    }
+
+    public static void addNewProductFirst(String product, LinkedList<String> productList) {
+        productList.addFirst(product);
+        System.out.println("продукт додано");
+    }
+
+    public static void buyProductAndDelete(LinkedList<String> productList) {
+        System.out.println("продукт" + productList.getFirst() + " був купленний");
+        productList.removeFirst();
+    }
+
+    public static void printAllProducts(LinkedList<String> productList) {
+        for (String product : productList) {
+            System.out.println(product);
+        }
+    }
+
+    public static void addProductByPosition(LinkedList<String> productList, int position, String name) {
+        productList.add(position, name);
+    }
+
+    public static void removeByName(LinkedList<String> productList, String name) {
+        productList.remove(name);
+    }
+}
+
+
+
+
